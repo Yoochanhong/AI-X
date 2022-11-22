4d4add86346a6413fbb4acbc49a5e0474e625f4c
Yoochanhong <chanhongy6@naver.com>
2022-11-22 19:23:21 +0900

    post 함수 생성

--- ai_x/lib/main.dart
+++ ai_x/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 void main() => runApp(MyApp());
 
@@ -22,12 +24,16 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final ImagePicker _picker = ImagePicker();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
+          onPressed: () {
+            postApi();
+          },
           child: Text('버튼'),
         ),
       ),
@@ -35,3 +41,18 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
+Future<dynamic> postApi(dynamic input) async {
+  var dio = new Dio();
+  try {
+    dio.options.contentType = 'multipart/form-data';
+    dio.options.maxRedirects.isFinite;
+    var response = await dio.patch(
+      'http://192.168.0.70:5001/fileUpload',
+      data: input,
+    );
+    print('성공적으로 업로드했습니다');
+    return response.data;
+  } catch (e) {
+    print(e);
+  }
+}
