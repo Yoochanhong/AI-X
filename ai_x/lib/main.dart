362328a7aa2fc8398faffae7eb0b2fe92ea1d97a
Yoochanhong <chanhongy6@naver.com>
2022-11-24 23:32:46 +0900

    플로팅 카메라 버튼 추가

--- ai_x/lib/main.dart
+++ ai_x/lib/main.dart
@@ -1,6 +1,4 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 void main() => runApp(MyApp());
 
@@ -24,81 +22,14 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  final viewModel = MainViewModel();
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            viewModel.isLoading
-                ? const CircularProgressIndicator()
-                : Container(),
-            ElevatedButton(
-              onPressed: () async {
-                setState(() {
-                  viewModel.isLoading = true;
-                });
-
-                final bytes =
-                    await rootBundle.load('assets/1111111111111111.jpeg');
-                await viewModel.uploadImage(bytes.buffer.asUint8List());
-
-                setState(() {
-                  viewModel.isLoading = false;
-                });
-              },
-              child: const Text('업로드 파일'),
-            ),
-          ],
-        ),
+      body: Center(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.camera_alt_sharp),
       ),
     );
-  }
-}
-
-class FileApi {
-  final _dio = Dio();
-
-  Future<Response> uploadImage(
-    Uint8List image,
-  ) async {
-    final formData = FormData.fromMap({
-      'file': MultipartFile.fromBytes(image.buffer.asUint8List(),
-          filename: '1111111111111111.jpeg'),
-    });
-
-    final response = await _dio.post(
-      'http://192.168.0.70:5001/fileUpload',
-      data: formData,
-    );
-
-    return response;
-  }
-}
-
-class FileRepository {
-  final _fileApi = FileApi();
-
-  Future<bool> uploadImage(Uint8List image) async {
-    try {
-      await _fileApi.uploadImage(image);
-      return true;
-    } catch (e) {
-      print(e.toString());
-      return false;
-    }
-  }
-}
-
-class MainViewModel {
-  final _repository = FileRepository();
-
-  var isLoading = false;
-
-  Future uploadImage(Uint8List image) async {
-    await _repository.uploadImage(image);
   }
 }
