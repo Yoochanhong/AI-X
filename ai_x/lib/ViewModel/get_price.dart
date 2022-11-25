36d1d351d6b7c6b2e776fc4fab2b867010ad55f5
Yoochanhong <chanhongy6@naver.com>
2022-11-25 23:14:50 +0900

    견적 Json 데이터를 가져오는 함수

---
+++ ai_x/lib/ViewModel/get_price.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+import 'package:ai_x/Model/price.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+
+Future<Price> getFollowApi() async {
+  final response = await http.get(Uri.parse('http://192.168.50.219:5001/printed'));
+  print(response.body);
+  if (response.statusCode == 200) {
+    return Price.fromJson(jsonDecode(response.body));
+  } else {
+    throw Exception('예외');
+  }
+}
