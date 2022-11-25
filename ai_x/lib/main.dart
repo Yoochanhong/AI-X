aea225926b45e9b27344e98f422fdbfa07678f0f
Yoochanhong <chanhongy6@naver.com>
2022-11-26 03:37:08 +0900

    중간 저장

--- ai_x/lib/main.dart
+++ ai_x/lib/main.dart
@@ -8,6 +8,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 
+enum CarSize { LightCar, CompectCar, MiddleCar, LargeCar }
+
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -36,28 +38,104 @@ class _MyHomePageState extends State<MyHomePage> {
   final picker = ImagePicker();
   XFile? image;
   String fileUploadUrl = 'http://192.168.50.219:5001/fileUpload';
+  String intValueUploadUrl = 'http://192.168.50.219:5001/intUpload';
   bool isUplode = true;
+  CarSize? carSize = CarSize.LightCar;
+
+  Map<Enum, String> carValueInt = {
+    CarSize.LightCar: '0',
+    CarSize.CompectCar: '1',
+    CarSize.MiddleCar: '2',
+    CarSize.LargeCar: '3',
+  };
+
+  String getEnumIndex(Enum? index) {
+    String enumIndex = carValueInt[index]!;
+    return enumIndex;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: image != null
-            ? Stack(
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Center(
-                    child: SizedBox(
-                      width: 6.0.w,
-                      height: 6.0.h,
-                      child: Image.file(
-                        File(image!.path),
-                        fit: BoxFit.fill,
+                  Stack(
+                    children: [
+                      Center(
+                        child: SizedBox(
+                          width: 6.0.w,
+                          height: 6.0.h,
+                          child: Image.file(
+                            File(image!.path),
+                            fit: BoxFit.fill,
+                          ),
+                        ),
                       ),
-                    ),
+                      isUplode == false
+                          ? Center(child: CircularProgressIndicator())
+                          : Container(),
+                    ],
                   ),
-                  isUplode == false
-                      ? const Center(child: CircularProgressIndicator())
-                      : Container(),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                        children: [
+                          Text('경차'),
+                          Radio<CarSize>(
+                              value: CarSize.LightCar,
+                              groupValue: carSize,
+                              onChanged: (CarSize? value) {
+                                setState(() {
+                                  carSize = value;
+                                });
+                              }),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('소형'),
+                          Radio<CarSize>(
+                              value: CarSize.CompectCar,
+                              groupValue: carSize,
+                              onChanged: (CarSize? value) {
+                                setState(() {
+                                  carSize = value;
+                                });
+                              }),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('중형'),
+                          Radio<CarSize>(
+                              value: CarSize.MiddleCar,
+                              groupValue: carSize,
+                              onChanged: (CarSize? value) {
+                                setState(() {
+                                  carSize = value;
+                                });
+                              }),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text('대형'),
+                          Radio<CarSize>(
+                              value: CarSize.LargeCar,
+                              groupValue: carSize,
+                              onChanged: (CarSize? value) {
+                                setState(() {
+                                  carSize = value;
+                                });
+                              }),
+                        ],
+                      ),
+                    ],
+                  ),
                 ],
               )
             : DottedBorder(
@@ -114,14 +192,29 @@ class _MyHomePageState extends State<MyHomePage> {
     });
     print("사진을 서버에 업로드 합니다.");
     var dio = Dio();
+    var dioInt = Dio();
     var formData =
         FormData.fromMap({'file': await MultipartFile.fromFile(input.path)});
+    var formDataInt = FormData.fromMap({'num': getEnumIndex(carSize)});
+    print(getEnumIndex(carSize));
     try {
       dio.options.contentType = 'multipart/form-data';
       dio.options.maxRedirects.isFinite;
       var response = await dio.post(
         fileUploadUrl,
         data: formData,
+      );
+    } catch (e) {
+      print(e);
+    }
+    setState(() {
+      isUplode = true;
+    });
+    try {
+      dioInt.options.contentType = 'text/plain';
+      var responseInt = await dioInt.post(
+        intValueUploadUrl,
+        data: formDataInt,
       );
       print('성공적으로 업로드했습니다');
       setState(() {
